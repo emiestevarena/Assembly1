1ef66c5506a9b9655cb0b62eddcd6754c6c6ad25
emiestevarena <emiliano.estevarena@gmail.com>
2020-09-09 07:59:13 -0300

    learning $-

--- hello.asm
+++ hello.asm
@@ -5,8 +5,9 @@ SYS_WRITE equ 1
 
 section .data
 	askName db "Enter name",10
+	askName_l equ $-askName
 	text db "Hi, "
-	exc db "!",10
+		
 
 section .bss
 	inName resb 16
@@ -27,7 +28,6 @@ _printHi:
 	call _eNameB
 	call _printHiA
 	call _printHiB
-	call _printHiC
 	ret
 	
 _printHiA:
@@ -47,21 +47,12 @@ _printHiB:
 	syscall
 	ret
 
-_printHiC:
-
-	mov rax, SYS_WRITE
-	mov rdi, 1
-	mov rsi, exc
-	mov rdx, 2
-	syscall
-	ret
-
 _eNameA:
 
 	mov rax, SYS_WRITE
 	mov rdi, 1
 	mov rsi, askName
-	mov rdx, 11
+	mov rdx, askName_l
 	syscall
 	ret
 
